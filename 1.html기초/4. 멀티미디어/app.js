137c19d719341d3fedff03f86a34fd457d4e6927
yn0315 <yunu0315@naver.com>
2021-06-15 16:25:28 +0900

    숙제

---
+++ 1.html기초/4. 멀티미디어/app.js
@@ -0,0 +1,6 @@
+const $btn = document. getElementById('change');
+const $petDiv = document. getElementById('my-pet');
+
+$btn.onclick = e => {
+    $petDiv.textContent = '못된 거북이!!';
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+## Html 복습용
+리셋태그`  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css">`
+
